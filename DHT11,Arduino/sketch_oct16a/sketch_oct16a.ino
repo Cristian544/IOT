20377986e95f4d5b4d0043c070424876320b8f2f
Cristian Hernandez <crizh221@gmail.com>
2024-10-16 21:44:12 -0500

    DHT11

---
+++ DHT11,Arduino/sketch_oct16a/sketch_oct16a.ino
@@ -0,0 +1,29 @@
+#include "DHT.h"
+#define DHTTYPE DHT11
+#define DHTPIN 12
+
+float temp = 0;
+float hum = 0;
+
+DHT dht(DHTPIN,DHTTYPE);
+
+void setup() {
+  
+  dht.begin();
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+  temp = dht.readTemperature();
+  hum = dht.readHumidity();
+
+  Serial.print("temperatura:  ");
+  Serial.println(temp);
+  Serial.print("Humendad:  ");
+  Serial.println(hum);
+
+  delay(2000);
+
+}
